bd70cfb31e07f0b6a19466564e1ee998e581487b
Javi Coca <javicc82@gmail.com>
2015-08-02 21:02:48 +0200

    Ejercicio Modulo 7: Search

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -13,12 +13,19 @@ exports.load = function(req, res, next, quizId) {
 };
 
 // GET /quizes
-exports.index = function(req, res) {
-  models.Quiz.findAll().then(
-    function(quizes) {
-      res.render('quizes/index', { quizes: quizes});
-    }
-  ).catch(function(error) { next(error);})
+exports.index = function(req, res){
+	if(req.query.search) {
+		var filtro = (req.query.search || '').replace(" ", "%");
+		models.Quiz.findAll({where:["pregunta like ?", '%'+filtro+'%'],order:'pregunta ASC'}).then(function(quizes){
+		res.render('quizes/index', {quizes: quizes});
+		}).catch(function(error) { next(error);});
+
+	} else {
+
+		models.Quiz.findAll().then(function(quizes){
+		res.render('quizes/index', {quizes: quizes});
+		}).catch(function(error) { next(error);});
+	}
 };
 
 // GET /quizes/:id
