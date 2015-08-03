ae62328cd855d2366107ad311b9b7510ba5da44c
Javi Coca <javicc82@gmail.com>
2015-08-03 18:20:11 +0200

    Validación

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var quizController = require('../controllers/quiz_controller');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Quiz' });
+  res.render('index', { title: 'Quiz', errors: []});
 });
 
 // Autoload de comandos con :quizId
@@ -13,6 +13,7 @@ router.param('quizId', quizController.load);  // autoload :quizId
 
 // Definición de rutas de /quizes
 router.get('/quizes', 	quizController.index);
+router.get('/author',   quizController.author);
 router.get('/quizes/:quizId(\\d+)', 	quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', 	quizController.answer);
 router.get('/quizes/new',                  quizController.new);
